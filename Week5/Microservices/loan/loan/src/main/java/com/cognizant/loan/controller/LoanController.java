b718dc90de4bedcee3890afbfd660cb99fc821dc
sujithamary <sujitha2101mary@gmail.com>
2025-07-20 23:28:28 +0530

    Week 5 Task Completed

---
+++ Week5/Microservices/loan/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,25 @@
+package com.cognizant.loan.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Map<String, Object> getLoan(@PathVariable String number) {
+        return Map.of(
+            "number", number,
+            "type", "car",
+            "loan", 400000,
+            "emi", 3258,
+            "tenure", 18
+        );
+    }
+}
+
